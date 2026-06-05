90ff2adbaeb7a7f89a8f6958ee982c71eda6b104
ColJops <jops24@o2.pl>
2026-06-06 01:03:17 +0200

    0.0.1 -> Ekran testowy

--- core/src/main/java/pl/dkupracz/legendsofunknow/LegendsOfUnknow.java
+++ core/src/main/java/pl/dkupracz/legendsofunknow/LegendsOfUnknow.java
@@ -1,32 +1,25 @@
 package pl.dkupracz.legendsofunknow;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class LegendsOfUnknow extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        Gdx.app.log("LegendsOfUnknow", "Game created and started");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        Gdx.gl.glClearColor(0.03f, 0.025f, 0.04f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        Gdx.app.log("LegendsOfUnknow", "Game disposed");
     }
 }
